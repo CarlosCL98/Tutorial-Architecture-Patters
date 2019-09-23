db77fc27daaadc36e168f62d2b2292b7b6b66d58
CarlosCL98 <camer.cl98@gmail.com>
2019-09-23 08:50:18 -0500

    Creación inicial del proyecto.

---
+++ src/main/java/edu/eci/tutorial/ArchitecturePatters/ArchitecturePattersApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.tutorial.ArchitecturePatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturePattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecturePattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/tutorial/ArchitecturePatters/ArchitecturePattersApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.eci.tutorial.ArchitecturePatters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArchitecturePattersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
